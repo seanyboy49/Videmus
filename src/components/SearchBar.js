71a16aeaea96f91fe2062d203aa0a79ee9ae146e
Sean Lee <seanboramlee@gmail.com>
2017-01-25 13:42:39 -0800

    add declarative binding to SearchBar state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
-class SearchBar extends Component{
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: 'Starting Value'
+    }
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState( {term: event.target.value })} />
+        Value of the input: {this.state.term}
+      </div>
+    );
   }
 
 
