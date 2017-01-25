0c9c20bfa19ec95a5079f78714b7dacde9d4a1c9
Sean Lee <seanboramlee@gmail.com>
2017-01-25 13:17:17 -0800

    refactor searchBar component from a functtional component into a React class

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import SearchBar from './components/SearchBar'
+import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
 
-const API_KEY = 'AIzaSyAbu2j5z6K3Mqreck1CEm_ct3hOLaJh4ww'
+const API_KEY = 'AIzaSyAbu2j5z6K3Mqreck1CEm_ct3hOLaJh4ww';
 
 // Create a new component. This component should produce some HTML
 
 const App = () => {
-  return <div>
-    <SearchBar />
-  </div>;
-
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this compoent's generated HTML and put it on the page (in the dom)
