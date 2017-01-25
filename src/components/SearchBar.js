6679cd9dd46f6546a7d069adaf9ed6cb270e29fe
Sean Lee <seanboramlee@gmail.com>
2017-01-25 13:26:23 -0800

    refactor onChange method into a one line fat arrow

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component{
   render() {
-    return <input />;
+    return <input onChange={event => console.log(event.target.value)} />;
   }
+
+
 }
 
 export default SearchBar;
